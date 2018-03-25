f1ed80d2a9af5dd418a3b0975eaa39cff8874b3d
Charles Francoise <charles.francoise@gmail.com>
2018-03-25 23:22:03 +0200

    Add logging

--- rebump.go
+++ rebump.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"os"
+
 	"github.com/loderunner/rebump/server"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.SetOutput(os.Stdout)
+
 	grpcAddress := ":8080"
 	restAddress := ":8081"
-	go server.ListenAndServeREST(restAddress, grpcAddress)
-	server.ListenAndServeGRPC(grpcAddress)
+
+	go func() {
+		log.Infof("Serving REST API on \"%s\"", restAddress)
+		err := server.ListenAndServeREST(restAddress, grpcAddress)
+		log.Fatalf("Failed to serve REST API: %s", err)
+	}()
+
+	log.Infof("Serving gRPC API on \"%s\"", grpcAddress)
+	err := server.ListenAndServeGRPC(grpcAddress)
+	log.Fatalf("Failed to serve gRPC API: %s", err)
 }
