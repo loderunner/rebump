0e3a03722576a1b026fc2d2bd673e3c8674f8608
Charles Francoise <charles.francoise@gmail.com>
2018-03-26 01:30:29 +0200

    Add createbump handler

--- server/handlers.go
+++ server/handlers.go
@@ -1,16 +1,32 @@
 package server
 
 import (
-	"golang.org/x/net/context"
+	"context"
+	"encoding/base64"
+	"path"
+
+	"github.com/google/uuid"
 
 	"github.com/loderunner/rebump/api"
 )
 
+const (
+	ResourceNameBump = "bump"
+)
+
 type Server struct{}
 
-func (srv *Server) Echo(ctx context.Context, req *api.EchoRequest) (res *api.EchoResponse, err error) {
-	res = &api.EchoResponse{
-		Message: req.Message,
+func base64UUID(id *uuid.UUID) string { return base64.RawURLEncoding.EncodeToString(id[:]) }
+
+func (srv *Server) CreateBump(ctx context.Context, req *api.CreateBumpRequest) (res *api.Bump, err error) {
+
+	id := uuid.New()
+	key := uuid.New()
+
+	res = &api.Bump{
+		Name:     path.Join(ResourceNameBump, base64UUID(&id)),
+		Location: req.Location,
+		Secret:   &api.Bump_Secret{Key: base64UUID(&key)},
 	}
 
 	return res, nil
