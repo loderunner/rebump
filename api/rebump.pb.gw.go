789a7980985928fdee7339d75b5d3f8e2613ae87
Charles Francoise <charles.francoise@gmail.com>
2018-03-26 17:09:03 +0200

    add GetNearby api

--- api/rebump.pb.gw.go
+++ api/rebump.pb.gw.go
@@ -43,6 +43,23 @@ func request_Rebump_CreateBump_0(ctx context.Context, marshaler runtime.Marshale
 
 }
 
+var (
+	filter_Rebump_GetNearby_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_Rebump_GetNearby_0(ctx context.Context, marshaler runtime.Marshaler, client RebumpClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetNearbyRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_Rebump_GetNearby_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetNearby(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterRebumpHandlerFromEndpoint is same as RegisterRebumpHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterRebumpHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -110,13 +127,46 @@ func RegisterRebumpHandlerClient(ctx context.Context, mux *runtime.ServeMux, cli
 
 	})
 
+	mux.Handle("GET", pattern_Rebump_GetNearby_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Rebump_GetNearby_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Rebump_GetNearby_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_Rebump_CreateBump_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"bump"}, ""))
+
+	pattern_Rebump_GetNearby_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"bump"}, "nearby"))
 )
 
 var (
 	forward_Rebump_CreateBump_0 = runtime.ForwardResponseMessage
+
+	forward_Rebump_GetNearby_0 = runtime.ForwardResponseMessage
 )
